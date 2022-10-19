cb6f4f33d8d013393ce8331c2ba76ab244da48b9
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-10-19 07:44:36 +0530

    POST Requests and Middlewares

--- index.js
+++ index.js
@@ -19,6 +19,31 @@ const friends =[
     }
 ]
 
+app.use((req, res, next) =>{
+    const start  = Date.now()
+    next();
+    const delta = Date.now() - start
+    console.log(`${req.method}  ${req.url}`);
+    console.log(delta);
+})
+
+app.use(express.json())
+
+app.post('/friends',(req,res) =>{
+
+    if(!req.body.name){
+      return res.status(400).json({
+        error : "Missing Friend Name"
+       })
+    }
+    const newFriend ={
+        name : req.body.name,
+        id : friends.length
+    };
+
+    friends.push(newFriend);
+    res.json(newFriend)
+})
 app.get('/friends',(req, res)=>{
 
   res.json(friends)
